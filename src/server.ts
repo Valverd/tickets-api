7cb8b413b5f5e957d6bd095d5005348882a66f1a
Renan Valverde <renanvgmartisn@hotmail.com>
2025-02-12 22:51:32 +0000

    Configurações do server e banco

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,17 @@
-import express from 'express'
-import mysql from 'mysql2'
+import express from "express";
+import { db } from "./db/db";
 
-const app = express()
-app.use(express.json())
-
-const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-})
+const app = express();
+app.use(express.json());
 
 db.connect((error) => {
-    if(error) {
-        console.log(error)
-        return
-    }
-    console.log('Connected to database')
-})
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log("Connected to database");
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
