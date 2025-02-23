bdc207f2e8a54170b5de6cf4980a80b0f4c6ea11
renan.valverde <renanvgmartins@hotmail.com>
2025-02-23 17:42:21 -0300

    Rota de compra finalizada, event clear lugares reservados no SQL criados

--- src/controllers/ticket_controller.ts
+++ src/controllers/ticket_controller.ts
@@ -1,25 +1,31 @@
 import { AuthenticatedRoutesHandler } from "../types/request_types";
 import { Response } from "express";
-import jwt from 'jsonwebtoken'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import Place from "../models/Place";
 
-export const buy_ticket: AuthenticatedRoutesHandler = async (req, res) => {
+export const buy_ticket: AuthenticatedRoutesHandler<{}, { place_id: string}> = async (req, res) => {
+    const { place_id } = req.body
+    const place_token = req.headers.place_token
+
     try {
-
-        const place_token = req.headers.place_token
-        if (!place_token) return res.status(404).json({ message: "Token de compra negado." })
+        if (!place_id) return res.status(404).json({ message: "Lugar não encontrado." })
+        if (!place_token) return res.status(404).json({ message: "Token não encontrado." })
 
         const place_token_verified = await jwt.verify(place_token, process.env.TOKEN_SECRET)
-        console.log(place_token)
-        console.log(place_token_verified)
-        res.send("teste")
+
+        if (place_id != (place_token_verified as JwtPayload).place_id) return res.status(400).json({ message: "ID de token não condiz com o lugar." })
+
+        await Place.update({ status: "occupied" }, { where: { id: place_id } })
+        res.json({ message: "Ingresso comprado" })
 
     } catch (error) {
-        handleError(res, error)
+        handleError(res, error, place_id)
     }
 }
 
-const handleError = (res: Response, error: unknown) => {
+const handleError = async (res: Response, error: unknown, place_id: string) => {
     if (error instanceof Error) {
+        if (error.message == "jwt expired") await Place.update({ status: "available" }, { where: { id: place_id } })
         return res.status(500).json({ message: error.message })
     } else {
         return res.status(500).json({ message: "Erro desconhecido", error })
